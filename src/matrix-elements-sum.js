a5f25c37c90eb66ec0840c3910a6c3437b32a685
Lyasikzzz <dyadyapavliklyas@gmail.com>
2023-04-18 00:47:18 +0300

    feat: add functions

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+/*   for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0;)
+  } */
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const heights = arr.filter(height => height !== -1).sort((a, b) => a - b);
+  let sortedHeightsIndex = 0;
+  return arr.map(height => {
+    if (height === -1) {
+      return -1;
+    } else {
+      const sortedHeight = heightssortedHeightsIndex;
+      sortedHeightsIndex++;
+      return sortedHeight;
+    }
+  });
 }
 
 module.exports = {
